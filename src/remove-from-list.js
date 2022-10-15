46712f8ab9b3f2fe89f5429d467a9c438e5d71ea
Masha0701 <reflection2008@gmail.com>
2022-10-16 01:53:39 +0300

    feat: k one of the tests

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,16 @@ const { ListNode } = require('../extensions/list-node.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList( {value, next}, k ) {
+  console.log(value, next, k);
+  if(value === k){
+    return next ? removeKFromList(next, k) : null;
+  } else {
+      return {
+        next : removeKFromList(next, k),
+        value
+      };
+  }
 }
 
 module.exports = {
